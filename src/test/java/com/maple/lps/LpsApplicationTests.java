1c5d7239d980353974558dd11f2ab381271e5c00
Antonius Rudi Sasangka <rudy2128@gmail.com>
2022-10-17 17:20:55 +0700

    Disable test

--- src/test/java/com/maple/lps/LpsApplicationTests.java
+++ src/test/java/com/maple/lps/LpsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.maple.lps;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LpsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.maple.lps;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LpsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
