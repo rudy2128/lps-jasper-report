a64c14d66829063a2255125e4ddd929a13b1eddf
Antonius Rudi Sasangka <rudy2128@gmail.com>
2022-10-21 18:20:54 +0700

    allow cors2

--- src/main/java/com/maple/lps/controllers/BankController.java
+++ src/main/java/com/maple/lps/controllers/BankController.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 @RestController
+@CrossOrigin(origins = "http://192.168.18.211:8787")
 public class BankController {
     private List<String>objects = new ArrayList<>();
 
